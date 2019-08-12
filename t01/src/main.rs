0d6b672044fc8a3b4a2d9e6d7a8c738283f33ab3
Michael Angerman <stormasm@gmail.com>
2019-08-12 07:35:32 -0700

    starting to now understand unwrap

--- t01/src/main.rs
+++ t01/src/main.rs
@@ -2,7 +2,7 @@ use sled::{Db, IVec};
 
 fn main() {
     let tree = Db::start_default("my_db").unwrap();
-    tree.set(b"yo!", b"v1".to_vec());
+    tree.set(b"yo!", b"v1".to_vec()).unwrap();
     assert_eq!(tree.get(b"yo!"), Ok(Some(IVec::from(b"v1"))));
-    tree.flush();
+    tree.flush().unwrap();
 }
