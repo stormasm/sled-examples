d038f4e4a6d00bae6217bd2266e90fbba3ab691b
Michael Angerman <stormasm@gmail.com>
2019-08-13 10:54:08 -0700

    ex03

--- t03/examples/ex03.rs
+++ t03/examples/ex03.rs
@@ -2,7 +2,11 @@ use sled::{Db, IVec};
 
 fn main() {
     let tree = Db::open("my_db").unwrap();
-    tree.insert(b"rick", b"v1".to_vec()).unwrap();
-    assert_eq!(tree.get(b"rick"), Ok(Some(IVec::from(b"v1"))));
+    tree.insert(b"rick", b"123 456 789".to_vec()).unwrap();
+    assert_eq!(tree.get(b"rick"), Ok(Some(IVec::from(b"123 456 789"))));
+
+    let result = tree.get(b"rick");
+    println!("{:?}", result);
+
     tree.flush().unwrap();
 }
