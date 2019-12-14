456d33afb39efd8152413ff602bb588813314d7b
Michael Angerman <stormasm@gmail.com>
2019-12-13 21:41:56 -0500

    cc

--- t01/examples/ex05json.rs
+++ t01/examples/ex05json.rs
@@ -7,12 +7,8 @@ fn main() {
     assert_eq!(tree.get(b"rick"), Ok(Some(IVec::from(vec![1, 2, 3]))));
 
     let expected = r#"{"One":1,"Two":2}"#;
-    tree.insert(b"rick", expected).unwrap();
-    /*
-        assert_eq!(tree.get(b"rick"), Ok(Some(expected)));
-    */
-
-    assert_eq!(tree.get(b"rick"), Ok(Some(IVec::from(expected))));
+    tree.insert(b"storm", expected).unwrap();
+    assert_eq!(tree.get(b"storm"), Ok(Some(IVec::from(expected))));
 
     tree.flush().unwrap();
 }
