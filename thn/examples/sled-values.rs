015f3c30478ae71d242ae02caf14720737b5dd3b
Michael Angerman <stormasm@gmail.com>
2019-12-15 18:37:42 -0500

    sled-values first cut

---
+++ thn/examples/sled-values.rs
@@ -0,0 +1,69 @@
+// https://doc.rust-lang.org/rust-by-example/scope/lifetime/methods.html
+
+use std::env;
+use std::process;
+use std::string::String;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use sled::Db;
+use std::convert::TryInto;
+use std::io::{BufRead, Error};
+
+use std::str::from_utf8;
+
+#[derive(Debug)]
+struct SledToVec<'a> {
+    key: &'a mut Vec<u32>,
+    value: &'a mut Vec<String>,
+}
+
+impl<'a> SledToVec<'a> {
+    fn is_even(num: u32) -> bool {
+        (num) & 1 == 0
+    }
+
+    fn write_json_to_sled(key: String, data: String) -> Result<(), Error> {
+        let tree = Db::open("sledb_hn").unwrap();
+        let _x = tree.insert(key, data.as_str().as_bytes());
+        tree.flush().unwrap();
+        Ok(())
+    }
+
+    fn readdb(&mut self, dbname: String) -> Result<(), Error> {
+        let tree = Db::open(dbname).unwrap();
+        let keys = tree.iter().keys();
+
+        for key in keys {
+            println!("{:?}", from_utf8(&key.unwrap()));
+        }
+
+        tree.flush().unwrap();
+        Ok(())
+        /*
+                for i in 0..self.key.len() {
+                    // println!("{} {}", self.key[i], self.value[i]);
+                    let _x = SledToVec::write_json_to_sled(self.key[i].to_string(), self.value[i].clone());
+                }
+        */
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("You need to enter a db name");
+        process::exit(1);
+    }
+    let dbname = &args[1];
+    println!("In file {}", dbname);
+
+    // Instantiate a SledToVec
+    let mut ftv: SledToVec = SledToVec {
+        key: &mut Vec::new(),
+        value: &mut Vec::new(),
+    };
+
+    let _contents = SledToVec::readdb(&mut ftv, dbname.to_string());
+}
