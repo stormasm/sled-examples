6cc7c791e858f671711e52bf8aa94acbb7ee144a
Michael Angerman <stormasm@gmail.com>
2019-12-15 11:47:47 -0500

    cc

--- thn/examples/sled-keys.rs
+++ thn/examples/sled-keys.rs
@@ -1,28 +1,13 @@
-// https://doc.rust-lang.org/rust-by-example/scope/lifetime/methods.html
-
-use std::env;
-use std::process;
-use std::string::String;
-
-use std::fs::File;
-use std::io::BufReader;
-
 use sled::Db;
-use std::convert::TryInto;
-use std::io::{BufRead, Error};
+use std::io::Error;
 
 fn get_keys() -> Result<(), Error> {
     let tree = Db::open("sledb_hn").unwrap();
-
-    // scan forward
-    let mut keys = tree.iter().keys();
-    // let key1 = iter.next().unwrap().unwrap();
+    let keys = tree.iter().keys();
 
     for key in keys {
         println!("{:?}", key);
     }
-
-    // println!("{:?}",key);
 
     tree.flush().unwrap();
     Ok(())
