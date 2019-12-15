03123fb8c0534b664afaa907c856496c9d2cfae6
Michael Angerman <stormasm@gmail.com>
2019-12-15 12:16:03 -0500

    its working with from_utf8

--- thn/examples/sled-keys.rs
+++ thn/examples/sled-keys.rs
@@ -1,12 +1,13 @@
 use sled::Db;
 use std::io::Error;
+use std::str::from_utf8;
 
 fn get_keys() -> Result<(), Error> {
     let tree = Db::open("sledb_hn").unwrap();
     let keys = tree.iter().keys();
 
     for key in keys {
-        println!("{:?}", key);
+        println!("{:?}", from_utf8(&key.unwrap()));
     }
 
     tree.flush().unwrap();
