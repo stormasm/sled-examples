4a46e5a73534422084f45f0005ace444a32e0e81
Michael Angerman <stormasm@gmail.com>
2020-12-04 20:53:48 -0800

    upgrade code

--- thn/examples/sled-keys.rs
+++ thn/examples/sled-keys.rs
@@ -1,9 +1,8 @@
-use sled::Db;
 use std::io::Error;
 use std::str::from_utf8;
 
 fn get_keys() -> Result<(), Error> {
-    let tree = Db::open("sledb_hn").unwrap();
+    let tree = sled::open("sledb_hn").unwrap();
     let keys = tree.iter().keys();
 
     for key in keys {
